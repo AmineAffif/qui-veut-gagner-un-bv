fd619714b0f91673e7304248a302c2e7e9fca94c
AmineAffif <affif.amine@live.fr>
2024-05-26 19:00:14 +0200

    (milhouse-game): confettis

--- app/games/milhouse/page.tsx
+++ app/games/milhouse/page.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import "app/styles/glasses.css";
+import JSConfetti from "js-confetti";
 
 function page() {
   const [glassesPosition, setGlassesPosition] = useState({ top: 0, left: 0 });
   const [isVisible, setIsVisible] = useState(false);
+  const [milhouseImage, setMilhouseImage] = useState("/milhouse_resized.webp");
 
   useEffect(() => {
     const windowWidth = window.innerWidth;
@@ -22,10 +24,20 @@ function page() {
     }, 550); // Délai avant de rendre l'image visible
   }, []);
 
+  const handleClick = () => {
+    console.log("Glasses clicked");
+
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti({
+      emojis: ["🌈", "🕶️", "⚡️", "👓", "✨", "💫", "🌸", "🕶️", "👓"],
+    });
+    setMilhouseImage("/milhouse_glasses_on_resized.webp");
+  };
+
   return (
     <div className="w-full lg:grid lg:grid-cols-2 flex items-center justify-center min-h-screen bg-sky_blue">
       <Image
-        src="/milhouse_resized.webp"
+        src={milhouseImage}
         alt="Milhouse without glasses"
         width="457"
         height="230"
@@ -37,12 +49,13 @@ function page() {
         alt="Glasses"
         width="100"
         height="50"
-        className={`absolute invert bg-black z-1 select-none ${isVisible ? "fade-in" : "hidden"}`}
+        className={`absolute z-1 select-none ${isVisible ? "fade-in" : "hidden"}`}
         style={{
           top: `${glassesPosition.top}px`,
           left: `${glassesPosition.left}px`,
         }}
         id="glasses"
+        onClick={handleClick}
       />
     </div>
   );
