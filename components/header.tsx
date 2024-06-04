b32d6a96c5d62f999af1a0405aefc361a7b4b23e
AmineAffif <affif.amine@live.fr>
2024-06-04 16:02:51 +0200

    fix margin header

--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
           <nav
             className={`${
               isMenuOpen ? "block" : "hidden"
-            } absolute top-16 left-0 w-full py-4 mt-4 bg-white md:bg-transparent md:relative md:top-0 md:flex md:items-center md:justify-start md:w-full md:background-white`}
+            } absolute top-16 left-0 w-full py-4 mt-4 md:mt-0 bg-white md:bg-transparent md:relative md:top-0 md:flex md:items-center md:justify-start md:w-full md:background-white`}
           >
             <Link
               className="m-4 invert block mr-4 md:inline-block h-9 w-max items-center justify-center rounded-md bg-transparent text-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
